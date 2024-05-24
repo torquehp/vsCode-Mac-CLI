bead308cd027f477afd0502d67ef6162d8b234ad
Furkan Demir <furkandemir@Furkan-MacBook-Air.local>
2024-05-24 17:40:32 +0300

    first commit mac vs code git CLI with terminal push

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hoş geldiniz!");
+        Console.WriteLine("Lütfen adınızı girin:");
+        string ad = Console.ReadLine();
+
+        Console.WriteLine("Merhaba, " + ad + "! Kaç yaşındasınız?");
+        int yas = Convert.ToInt32(Console.ReadLine());
+
+        if (yas < 18)
+        {
+            Console.WriteLine("Üzgünüz, bu programı kullanabilmek için 18 yaşından büyük olmalısınız.");
+        }
+        else
+        {
+            Console.WriteLine("Harika, " + ad + "! Programı kullanmaya devam edebilirsiniz.");
+            Console.WriteLine("Lütfen bir sayı girin:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz sayının karesi: " + (sayi * sayi));
+        }
+
+        Console.WriteLine("Programdan çıkmak için bir tuşa basın...");
+        Console.ReadKey();
+    }
+}
